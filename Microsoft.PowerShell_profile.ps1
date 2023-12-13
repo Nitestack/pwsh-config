b65e4555cab4b3281d3ede2e2a4abf55daa9a726
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-13 21:33:07 +0100

    init: configure git ignore and add pwsh config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,42 @@
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile))
+{
+  Import-Module "$ChocolateyProfile"
+}
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\emodipt-extend.omp.json" | Invoke-Expression
+
+Import-Module Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord "Ctrl+d" -Function DeleteChar
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Aliases
+Set-Alias grep findstr
+Set-Alias vim nvim.exe
+Set-Alias lvim 'C:\Users\hydra\.local\bin\lvim.ps1'
+
+# Utilities
+function which($command)
+{
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function Open-PowerShellConfig() {
+  nvim.exe $env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+}
+Set-Alias psc Open-PowerShellConfig
+
+Import-Module Nvim-Switcher
